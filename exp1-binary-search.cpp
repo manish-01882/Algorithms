993692fba0a442714932ec237a7aa2bc8d8b14e8
manish-01882 <man01882@outlook.com>
2024-08-02 13:12:16 +0530

    new file:   .vscode/tasks.json 	new file:   exp1-binary-search.cpp 	new file:   exp2-merge-sort 	new file:   exp2-merge-sort.cpp

---
+++ exp1-binary-search.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int binary_search(int arr[], int beg, int end, int value){
+    while (beg <= end){
+        int mid = (beg + (end - beg) / 2);
+        if (arr[mid] == value){
+            return mid;
+        }
+        else if (value > arr[mid]){
+            beg = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    cout << "Manish Choudhary\nURN: 2203495\n";
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter Roll No. in array: ";
+    for(int i=0; i<n; ++i)
+        cin>>arr[i];
+    int ele;
+    char c = 'y';
+    while(c == 'y' || 'Y'){
+        cout <<"Array to find: ";
+        cin >> ele;
+        int pos = binary_search(arr, 0, n - 1, ele);
+
+        if (pos != -1)
+            cout << "Roll No. is present at location: " << pos << endl;
+        else
+            cout << "Roll No. not found!";
+        cout << "Do you want to continue?(y/n): ";
+        cin >> c;
+    }
+    
+}
---
+++ exp2-merge-sort.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+
+void merge(int array[], int left, int middle,int right){
+    int n1 = middle-left+1;
+    int n2 = right-middle;
+
+    int *leftArray = new int[n1];
+    int *rightArray = new int[n2];
+
+    for(int i=0; i<n1; ++i)
+        leftArray[i] = array[left+i];
+    for(int i=0; i<n2; ++i)
+        rightArray[i] = array[middle+1+i];
+
+
+    int i=0;    //For left array
+    int j=0;    //For right array
+    int k=left;     //For main array
+
+    while(i<n1 && j<n2){
+        if(leftArray[i]<rightArray[j]){
+            array[k] = leftArray[i];
+            ++i;
+        }
+        else{
+            array[k] = rightArray[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while(i<n1){
+        array[k]=leftArray[i];
+        ++i;
+        ++k;
+    }
+    while(j<n2){
+        array[k]=rightArray[j];
+        ++j;
+        ++k;
+    }
+
+}
+
+void mergeSort(int array[], int left, int right){
+    if(left < right){
+        int middle = (left + right)/2;
+        mergeSort(array,left,middle);
+        mergeSort(array,middle+1, right);
+        merge(array,left,middle,right);
+    }
+}
+
+int main(){
+    int array[] = {1,3,5,9,7,2,8,4,6};
+    int size = sizeof(array) / sizeof(array[0]);
+
+    mergeSort(array, 0, size-1);
+    for(int i=0;i<size; ++i){
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
